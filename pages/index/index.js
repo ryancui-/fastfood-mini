e9789ea2c3b4cd931a36dec721cb6b5ce4d2da31
ryancui- <cuizhaowei92@gmail.com>
2017-11-08 14:33:58 +0800

    feat: 订单团查看页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,23 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+const host = require('../../config').host;
 
 Page({
   data: {
+    groups: []
   },
-  onLoad: () => {
-  
+  onLoad: function () {
+    this.setData({
+      groups: new Array(4).fill({
+        composer: {
+          nickname: 'DeLock'
+        },
+        status: 1,
+        totalPrice: 49,
+        dueTime: '2017-11-06 10:30:00',
+        avatar_url: 'https://wx.qlogo.cn/mmopen/vi_32/JPcCJiabb9uMWCAdHvBxFIiaLGQDyxVDKegr9TgBY078N4z4vEFxaPGUfwicwg5m1eFq1hjtGb9wz6BCUkk4AmKxQ/0'
+      })
+    });
   }
 })
