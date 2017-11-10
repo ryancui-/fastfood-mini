72202a7eb28ad275edc047c445584352004f10dd
ryancui- <cuizhaowei92@gmail.com>
2017-11-10 15:31:41 +0800

    feat: 菜单列表添加菜式到订单团

--- pages/list-product/list-product.js
+++ pages/list-product/list-product.js
@@ -1,4 +1,7 @@
 // pages/list-product/list-product.js
+const host = require('../../config').host;
+const app = getApp();
+
 Page({
 
   /**
@@ -8,11 +11,17 @@ Page({
     groupId: null,
     products: []
   },
+  still: true,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log('ListProduct.onLoad');
+    if (!options.groupId) {
+      wx.navigateBack();
+    }
+
     this.setData({
       groupId: options.groupId
     });
@@ -50,9 +59,12 @@ Page({
 
   // 到达底部，获取更多数据
   onReachBottom: function () {
-    this.getProductData();
+    if (this.still) {
+      this.getProductData();
+    }
   },
 
+  // 加载产品列表
   getProductData: function (init) {
     if (init) {
       this.setData({
@@ -69,42 +81,68 @@ Page({
       mask: true
     });
 
-    setTimeout(() => {
-      let result;
-      if (init) {
-        result = new Array(10).fill({
-          name: '盐焗手撕鸡',
-          price: 17
+    wx.request({
+      url: `${host}/api/product/list`,
+      method: 'POST',
+      data: {
+        page: this.data.page
+      },
+      header: {
+        'authorization': app.globalData.token
+      },
+      success: ({data}) => {
+        if (data.data.length === 0) {
+          this.still = false;
+        }
+
+        let result;
+        if (init) {
+          result = data.data;
+        } else {
+          result = this.data.products.concat(data.data);
+        }
+
+        this.setData({
+          products: result
         });
-      } else {
-        result = this.data.products.concat(new Array(10).fill({
-          name: '盐焗手撕鸡',
-          price: 17
-        }));
+
+        wx.hideLoading();
       }
-
-      this.setData({
-        products: result
-      });
-
-      wx.hideLoading();
-    }, 1000);
+    });
   },
 
+  // 添加产品到订单团
   selectProduct: function (e) {
     const product = e.currentTarget.dataset.product;
 
-    console.log(product);
+    const query = {
+      groupId: this.data.groupId,
+      productId: product.id
+    };
 
     wx.showModal({
       title: '提示',
       content: `是否下订${product.name}`,
       success: (res) => {
         if (res.confirm) {
-          console.log('用户点击确定')
-          wx.navigateBack();
-        } else if (res.cancel) {
-          console.log('用户点击取消')
+          wx.showLoading({
+            mask: true,
+            title: '处理中'
+          });
+
+          wx.request({
+            url: `${host}/api/order/create`,
+            method: 'POST',
+            data: query,
+            header: {
+              'authorization': app.globalData.token
+            },
+            success: ({data}) => {
+              console.log(data.data);
+              wx.hideLoading();
+              wx.navigateBack();
+            }
+          });
         }
       }
     });
